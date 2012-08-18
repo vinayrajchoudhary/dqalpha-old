0ff9343a9f1c1dd9a43441d330d66a1abb661761
vinayrajchoudhary <vinayrajchoudhary@gmail.com>
2012-08-19 01:13:52 +0530

    --mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,3 @@
 class UserMailer < ActionMailer::Base
-  default from: "stormy-escarpment-3556.herokuapp.com"
-  def welcome_email(user)
-    @user = user
-    @url  = "http://stormy-escarpment-3556.herokuapp.com/signin"
-    mail(:to => user.email, :subject => "Welcome to My Awesome Site")
-  end
+  
 end
